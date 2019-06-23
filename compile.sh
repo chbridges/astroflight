eeb9cae3a1637a04d3c76298b5abd814c09b3490
chbridges <christopher.brueckner95@gmail.com>
2019-06-23 20:23:53 +0200

    init

---
+++ compile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if test $# -eq 0
+then echo
+    echo "This shell-script compiles a <filename>.cpp and links "
+    echo "the X11, OpenGL and GLFW-Libs."
+    echo "If you get this message, check for the <filename>."
+    echo
+    echo "Usage: my_ccp (filename, without qualifier .cpp)"
+else if test ! -s "$1.cpp"
+    then echo "no file "$1.cpp" or "$1.cpp" is empty"
+    else echo "g++ -std=c++1z "$1.c" -lglfw3 -lGL -lX11 -lpthread -lXrandr -lXi -ldl "
+        g++ -std=c++1z glad.c "$1.cpp" -o $1 -lglfw3 -lGL -lX11 -lpthread -lXrandr -lXi -ldl
+        echo "strip $1"
+        strip $1
+        ./$1
+    fi
+fi      
